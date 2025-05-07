3670508c4c030634e8171679b9f2d5e94a5a1037
dxt <raf.giebultowski@gmail.com>
2025-05-07 16:51:05 +0200

    Dodaj pliki projektów.

---
+++ WebApplication1/Controllers/RepositoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class RepositoryController : ControllerBase
+    {
+        [HttpPost("archive")]
+        public async Task<IActionResult> ArchiveRepository([FromBody] CloneRequest request)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication1/Models/CloneRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class CloneRequest
+    {
+        [Required]
+        public string RepoUrl { get; set; }
+    }
+}

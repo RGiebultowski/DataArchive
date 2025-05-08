1a265f2988eb05dade6982726deed45c160867c7
dxt <raf.giebultowski@gmail.com>
2025-05-08 13:47:55 +0200

    added services that handles events

---
+++ WebApplication1/Services/GitRepoService.cs
@@ -0,0 +1,16 @@
+﻿using LibGit2Sharp;
+
+namespace WebApplication1.Services
+{
+    public class GitRepoService : IGitRepoService
+    {
+        public string CloneRepository(string repoUrl)
+        {
+            var path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(path);
+
+            Repository.Clone(repoUrl, path);
+            return path;
+        }
+    }
+}
---
+++ WebApplication1/Services/StoreService.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Services
+{
+    public class StoreService : IStoreService
+    {
+        public string UploadFile(string zippedFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

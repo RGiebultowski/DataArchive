c0db9d89410ac2b26c3087e6002f7a9605a7ca3f
dxt <raf.giebultowski@gmail.com>
2025-05-08 16:35:08 +0200

    added DI to program.cs

---
+++ WebApplication1/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1
+{
+    internal static class DependencyInjection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+            => services
+            .AddScoped<IGitRepoService, GitRepoService>();
+    }
+}

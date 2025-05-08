8d5580bfdd909ded768d1854c23a18bc7be5c588
dxt <raf.giebultowski@gmail.com>
2025-05-08 15:05:53 +0200

    filling controller

--- WebApplication1/Controllers/RepositoryController.cs
+++ WebApplication1/Controllers/RepositoryController.cs
@@ -1,14 +1,30 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
+using WebApplication1.Services;
 
 namespace WebApplication1.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class RepositoryController : ControllerBase
     {
+
+        private readonly IGitRepoService gitRepoService;
+
+        public RepositoryController(IGitRepoService gitRepoService)
+        {
+            this.gitRepoService = gitRepoService;
+        }
+
         [HttpPost("archive")]
         public async Task<IActionResult> ArchiveRepository([FromBody] CloneRequest request)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            gitRepoService.CloneRepository(request.RepoUrl);
+            await Task.CompletedTask;
             return Ok();
         }
     }
